e01fb3bfcb4a7631542c1f2769374c0747fbbbf8
Daxi01 <daxi01cz@gmail.com>
2024-07-21 18:33:04 +0200

    Update - removed doubled entry verification

--- Models/Station.cs
+++ Models/Station.cs
@@ -8,16 +8,12 @@ namespace FloodingApp.Models
     {
         public int Id { get; set; }
 
-        [Required(ErrorMessage = "Title is required")]
         public string Title { get; set; }
 
-        [Range(1, 10, ErrorMessage = "Flood Warning Value must be between 1 and 10")]
         public int FloodWarningValue { get; set; }
 
-        [Range(1, 10, ErrorMessage = "Drought Warning Value must be between 1 and 10")]
         public int DroughtWarningValue { get; set; }
 
-        [Range(1, 1440, ErrorMessage = "Time Tolerance must be between 1 and 1440 minutes")]
         public int TimeToleranceInMin { get; set; }
 
         public DateTime CreatedOn { get; set; }
